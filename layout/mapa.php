41e5ae4f5572c38cc2c56bb6737f412b9566e100
Pedro Onofre Santos <pedroo@ufmg.br>
2013-06-18 02:27:57 -0300

    Codigo Variavel
    power of php

--- layout/mapa.php
+++ layout/mapa.php
@@ -21,6 +21,48 @@
 	<script type='text/javascript' src="jquery/jquery-1.9.1.js"></script>
 	<script type='text/javascript' src='tipsy/javascripts/jquery.tipsy.js'></script>
     <script>
+    <?php
+    	$regiao = "meso";
+		$qr="SELECT id$regiao, count(*)
+		FROM `candidato_relatorio`
+		WHERE crel_processo = '20111_integrado'
+		AND idmeso <> \"\"
+		GROUP BY idmeso
+		order by count(*) desc;";
+		
+		$RES = mysql_query($qr) or die(mysql_error()); 
+		
+		$i=0;
+		while($row=mysql_fetch_array($RES)){
+		    $mesos[$i] = $row['id'.$regiao.''];
+		    $mesoat = $mesos[$i];
+		    $quantidade[$i] = $row[1];
+		   
+		    $qr2="SELECT avg(crel_notafinal) FROM candidato_relatorio WHERE id$regiao='$mesoat' AND crel_processo = '20111_integrado'";
+		    $RES2 = mysql_query($qr2) or die(mysql_error()); 
+		    $row2=mysql_fetch_array($RES2);
+		    $media[$i] = $row2[0];
+			
+			$qr3="SELECT count(*) FROM candidato_relatorio WHERE id$regiao='$mesoat' AND crel_processo = '20111_integrado' AND crel_sit_vestibular = 'C' ";
+		    $RES3 = mysql_query($qr3) or die(mysql_error()); 
+		    $row3=mysql_fetch_array($RES3);
+		    $candidatos_aprovados[$i] = $row3[0];
+		 
+		 
+		    $i++;
+		}
+		
+		echo 'var '.$regiao.'Array = [';
+		
+		for($i=0;$i<count($mesos);$i++){
+		   if($i!=0) echo ",\n\t\t\t\t";
+		   echo '{"id'.$regiao.'": "'.$mesos[$i].'","candidatos": '.$quantidade[$i].', "candidatos_aprovados": '.$candidatos_aprovados[$i].', "media": '.$media[$i].'}';
+		    
+		}
+		echo " ];\n";
+		?>
+		</script>
+		<script>
     	var center = [-44.31301959952854, -18.95937600486544];
 		var color = d3.scale.linear()
 		    .domain([.00,.20,.40])
@@ -318,7 +360,5 @@
 		}
 
     </script>
-<?php
-	require_once("system_footer.php");
-
+<?php require_once("system_footer.php");
 ?>
